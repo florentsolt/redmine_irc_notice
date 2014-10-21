d7dce904478733a2e479d511c5efc55a89f437cc
Florent Solt <florent@solt.biz>
2014-10-21 10:49:16 +0200

    Socket managment in a separate thread

--- lib/irc.rb
+++ lib/irc.rb
@@ -11,6 +11,7 @@ module Irc
     Rails.application.routes.url_helpers.url_for(param)
   end
 
+  @@mutex = Mutex.new
   @@socket = nil
   @@listen = {}
 
@@ -22,45 +23,41 @@ module Irc
     @@socket.puts "NOTICE #{Setting.plugin_redmine_irc_notice['chan'].strip} :#{message}"
   end
 
-  def self.connect
-    # Connect
-    begin
-      @@socket = TCPSocket.new(Setting.plugin_redmine_irc_notice['host'],
-        Setting.plugin_redmine_irc_notice['port'].to_i)
-      @@socket.sync = true
-    rescue
-      return
+  def self.notice(object)
+    Thread.new do
+      @@mutex.synchronize do
+        begin
+          if @@listen.key? object.class.name
+            @@socket = TCPSocket.new(Setting.plugin_redmine_irc_notice['host'], Setting.plugin_redmine_irc_notice['port'].to_i)
+
+            nick = object.class.name.split(':').last.downcase
+
+            @@socket.puts "NICK #{nick}"
+            @@socket.puts "USER #{nick} #{nick} #{nick} #{nick}"
+
+            # Wait register
+            begin
+              line = @@socket.gets
+            end while line !~ /001 #{nick}/
+
+            # CTCP Time (not sure about that)
+            @@socket.puts "NOTICE #{nick} #{Time.now}"
+
+            @@listen[object.class.name].call(object)
+          end
+        rescue => e
+          Rails.logger.error "Irc notification error: #{e.message}"
+        ensure
+          if not @@socket.nil? and @@socket
+            @@socket.puts "QUIT"
+            @@socket.flush
+            @@socket.gets until @@socket.eof?
+            @@socket.close
+            @@socket = nil
+          end
+        end
+      end
     end
   end
 
-  def self.notice(object)
-    self.connect
-    return if not @@listen.key? object.class.name
-    # Init
-    nick = object.class.name.split(':').last.downcase
-
-    @@socket.puts "NICK #{nick}"
-    @@socket.puts "USER #{nick} #{nick} #{nick} #{nick}"
-
-    # Wait register
-    begin
-      line = @@socket.gets
-    end while line !~ /001 #{nick}/
-
-    # CTCP Time (not sure about that)
-    @@socket.puts "NOTICE #{nick} #{Time.now}"
-
-    @@listen[object.class.name].call(object)
-    self.close
-  end
-
-  def self.close
-    return if @@socket.nil?
-    # Close
-    @@socket.puts "QUIT"
-    @@socket.flush
-    @@socket.gets until @@socket.eof?
-    @@socket.close
-    @@socket = nil
-  end
 end
