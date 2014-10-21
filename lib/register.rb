55c4e89d5373ad6e3de6ac928fdf696b3856c404
Florent Solt <florent@solt.biz>
2014-10-21 17:23:46 +0200

    Cleanup

--- lib/register.rb
+++ lib/register.rb
@@ -2,52 +2,29 @@ Irc.register "Changeset" do |changeset|
 
   issue = changeset.issues.first
 
-  first_line = "New changeset"
+  buf = "New changeset"
+  buf << " by #{changeset.user.login.titleize}" if not changeset.user.nil?
+  buf << " for issue \"#{issue.subject}\" in #{issue.project.name}" if not issue.nil?
 
-  if changeset.user.is_a? User
-    first_line << " by #{changeset.user.login.titleize}"
+  Irc.say buf
+
+  changeset.comments.split("\n").each do |line|
+    Irc.say "   » #{line.gsub('#', '')}"
   end
 
-  if not issue.nil?
-    first_line << " for issue \"#{issue.subject}\" in #{issue.project.name}"
-  end
-
-  Irc.say first_line
-
-  if not changeset.comments.gsub(/([r#]\d+)|merge|trunk|qa|prod|in|to/i, '').strip.empty?
-    changeset.comments.split("\n").each do |line|
-      Irc.say "   » #{line.gsub('#', '')}"
-    end
-  end
-
-  if not issue.nil?
-    url = Irc.url(issue.event_url)
-    Irc.say "   » #{url} (issue)"
-  end
-
-  url = Irc.url(changeset.event_url)
-  Irc.say "   » #{url}"
-
+  Irc.say "   » #{Irc.url(issue.event_url)}" if not issue.nil?
+  Irc.say "   » #{Irc.url(changeset.event_url)}"
 end
 
 Irc.register "Issue" do |issue|
 
-  first_line = "New action "
+  buf = "New issue"
+  buf << " by #{issue.author.login.titleize}" if not issue.author.nil?
+  buf << " for #{issue.assigned_to.login.titleize}" if not issue.assigned_to.nil?
+  buf << " in #{issue.project.name}"
 
-  if issue.author
-      first_line << " by #{issue.author.login.titleize}"
-  end
-
-  if issue.assigned_to
-      first_line << " for #{issue.assigned_to.login.titleize}"
-  end
-
-  first_line << " in #{issue.project.name}"
-
-  Irc.say first_line
+  Irc.say buf
   Irc.say "   » #{issue.subject}"
-
-  url = Irc.url(issue.event_url)
-  Irc.say "   » #{url}"
+  Irc.say "   » #{Irc.url(issue.event_url)}"
 
 end
